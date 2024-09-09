9a5321857ffd91ac5173209d5fa1b2ffbcd4f31c
DM2523 <dsingh1@ce.iitr.ac.in>
2024-09-09 17:12:58 +0530

    sidebar update

--- pages/4_Real_Time_Detection.py
+++ pages/4_Real_Time_Detection.py
@@ -1,12 +1,46 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer,WebRtcMode
 import av
-
+import os
+import logging
 from Inference import YOLO_PRED
 
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+
+logger = logging.getLogger(__name__)
+
+
 def get_ice_servers():
-    """Return a free STUN server from Google."""
-    return [{"urls": ["stun:stun4.l.google.com:19302"]}]
+    """Use Twilio's TURN server because Streamlit Community Cloud has changed
+    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+    See https://github.com/whitphx/streamlit-webrtc/issues/1213
+    """
+
+    # Ref: https://www.twilio.com/docs/stun-turn/api
+    try:
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    except KeyError:
+        logger.warning(
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    client = Client(account_sid, auth_token)
+
+    try:
+        token = client.tokens.create()
+    except TwilioRestException as e:
+        st.warning(
+            f"Error occurred while accessing Twilio API. Fallback to a free STUN server from Google. ({e})"  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    return token.ice_servers
+
 
 
 st.set_page_config(page_title = 'WebRTC Detection',
